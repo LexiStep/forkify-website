63fd23dc4fffc8c5fa831a5dc7637717cf3cc2be
LexiStep <wertyshka99@live.com>
2024-10-09 14:25:49 +0300

    added new f

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,6 +119,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandleRender(controlRecipes);
@@ -127,6 +131,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(contolPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
